2f4093b61375d12899a89ab864eda18aab4527f4
Nieraa <timeboy3@hotmail.com>
2022-12-21 17:19:40 +0700

    connect with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Project } from './entities/projects.entity';
+import { ProjectsController } from './projects.controller';
+import { ProjectsService } from './projects.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'localhost',
+      port: 27017,
+      database: 'uwb',
+      entities: [Project],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Project]),
+  ],
+  controllers: [AppController, ProjectsController],
+  providers: [AppService, ProjectsService],
 })
 export class AppModule {}
