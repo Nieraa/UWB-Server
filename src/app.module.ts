a478f7721f377338aee49c513b25b5a001302ee7
Nieraa <timeboy3@hotmail.com>
2023-02-17 20:01:30 +0700

    add auth module in to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 import { ProjectsModule } from './projects/projects.module';
 import { RoomPlansModule } from './roomPlans/roomPlans.module';
 import { AnchorsModule } from './anchors/anchors.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), ProjectsModule, RoomPlansModule, AnchorsModule],
+  imports: [ConfigModule.forRoot(), ProjectsModule, RoomPlansModule, AnchorsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
