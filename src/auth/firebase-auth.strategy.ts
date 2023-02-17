0bfc3603298da97eb665ebd6a051e0192a8761f3
Nieraa <timeboy3@hotmail.com>
2023-02-17 20:01:04 +0700

    add firebase_auth strategy

---
+++ src/auth/firebase-auth.strategy.ts
@@ -0,0 +1,23 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Strategy, ExtractJwt } from "passport-firebase-jwt";
+import { auth } from "firebase-admin";
+
+@Injectable()
+export class FirebaseAuthStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(token: string) {
+    try {
+      return await auth()
+        .verifyIdToken(token, true);
+    } catch (err) {
+      console.warn(err);
+      throw new UnauthorizedException();
+    }
+  }
+}
