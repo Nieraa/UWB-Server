62cd51ca7f327aec0e04e686999146ed2d233a77
Nieraa <timeboy3@hotmail.com>
2023-02-17 20:00:34 +0700

    use firebase functions config instead of env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
-import { ServiceAccount } from 'firebase-admin';
-
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as admin from 'firebase-admin';
 import * as express from 'express';
@@ -17,20 +14,16 @@ export const createFunction = async (expressInstance: express.Express) => {
     AppModule,
     new ExpressAdapter(expressInstance)
   );
-  const configService: ConfigService = app.get(ConfigService);
-  // Set the config options
 
-  const adminConfig: ServiceAccount = {
-    projectId: configService.get<string>('PROJECT_ID'),
-    privateKey: configService.get<string>('PRIVATE_KEY').replace(/\\n/g, '\n'),
-    clientEmail: configService.get<string>('CLIENT_EMAIL'),
-  };
   // Initialize the firebase admin app
-
   if (admin.apps.length === 0) {
     admin.initializeApp({
-      credential: admin.credential.cert(adminConfig),
-      databaseURL: `https://${adminConfig.projectId}-default-rtdb.asia-southeast1.firebasedatabase.app/`,
+      credential: admin.credential.cert({
+        projectId: functions.config().admin_firebase_config.project_id,
+        clientEmail: functions.config().admin_firebase_config.client_email,
+        privateKey: functions.config().admin_firebase_config.private_key.replace(/\\n/g, '\n'),
+      }),
+      databaseURL: `https://${functions.config().admin_firebase_config.project_id}-default-rtdb.asia-southeast1.firebasedatabase.app/`,
     });
   }
 
