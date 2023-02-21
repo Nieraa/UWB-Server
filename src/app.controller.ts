b437c4bd6c4c4bbc8f55ad64334bf4a9ed5e2ed6
Nieraa <timeboy3@hotmail.com>
2023-02-21 17:15:20 +0700

    add LocalGuard and signin method to app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/signin')
+  async signin(@Request() req) {
+    return req.user;
   }
 }
