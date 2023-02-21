7246e4e231aae938c962d5ae6ffd44d67d7567ff
Nieraa <timeboy3@hotmail.com>
2023-02-21 17:08:53 +0700

    add UsersService in user module exports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,6 +5,7 @@ import { UsersService } from './users.service';
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 
 export class UsersModule {}
