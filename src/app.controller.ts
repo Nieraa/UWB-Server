88cd246039b7b343903045c76a6e100c45e60c14
Nieraa <timeboy3@hotmail.com>
2023-02-21 17:21:27 +0700

    use signin authService in signin method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService,) { }
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService
+  ) { }
 
   @Get()
   getHello(): string {
@@ -14,6 +18,6 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('auth/signin')
   async signin(@Request() req) {
-    return req.user;
+    return this.authService.signin(req.user);
   }
 }
