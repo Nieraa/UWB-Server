0c965ee42d995dac881f3df1e7b0f9c48254d06a
Nieraa <timeboy3@hotmail.com>
2023-02-22 18:02:08 +0700

    import ProjectsModule and export RoomPlansService

--- src/roomPlans/roomPlans.module.ts
+++ src/roomPlans/roomPlans.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ProjectsModule } from 'src/projects/projects.module';
 import { RoomPlansController } from './roomPlans.controller';
 import { RoomPlansService } from './roomPlans.service';
 
 @Module({
+  imports: [ProjectsModule],
   controllers: [RoomPlansController],
   providers: [RoomPlansService],
+  exports: [RoomPlansService]
 })
 export class RoomPlansModule {}
