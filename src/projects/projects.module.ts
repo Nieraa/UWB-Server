d79286f3b3524878b3a92a69b1678e8557884096
Nieraa <timeboy3@hotmail.com>
2023-02-22 18:01:43 +0700

    export ProjectService

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -5,5 +5,6 @@ import { ProjectsService } from './projects.service';
 @Module({
   controllers: [ProjectsController],
   providers: [ProjectsService],
+  exports: [ProjectsService]
 })
 export class ProjectsModule {}
