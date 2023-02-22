f9b611d38f964abbc8a4faee4a678e081c216c41
Nieraa <timeboy3@hotmail.com>
2023-02-22 18:02:35 +0700

    import RoomPlansModule and export AnchorsService

--- src/anchors/anchors.module.ts
+++ src/anchors/anchors.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { RoomPlansModule } from 'src/roomPlans/roomPlans.module';
 import { AnchorsController } from './anchors.controller';
 import { AnchorsService } from './anchors.service';
 
 @Module({
+  imports: [RoomPlansModule],
   controllers: [AnchorsController],
   providers: [AnchorsService],
 })
